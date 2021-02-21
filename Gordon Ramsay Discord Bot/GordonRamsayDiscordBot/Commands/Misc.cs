96bdec0ff76c7a57544a68cb7871b000428f2d43
Iain <45532247+Arrowerse2001@users.noreply.github.com>
2021-02-21 23:54:42 +0000

    Added help menu

--- Gordon Ramsay Discord Bot/GordonRamsayDiscordBot/Commands/Misc.cs
+++ Gordon Ramsay Discord Bot/GordonRamsayDiscordBot/Commands/Misc.cs
@@ -37,5 +37,49 @@ namespace GordonRamsayBot.Commands
         // trivia
         [Command("trivia")]
         public async Task TryToStartTrivia(string input = null) => await Handlers.MinigameHandler.Trivia.TryToStartTrivia((SocketGuildUser)Context.User, Context, input ?? "trivia");
+    
+        // Bio
+        [Command("bio")]
+        public async Task DisplayGordonBio()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.WithTitle("Gordon Ramsay")
+                .WithDescription("Gordon James Ramsay OBE is a British chef, restaurateur, television personality, and writer. Born in Johnstone, Scotland, and raised in Stratford-upon-Avon, England, he founded his global restaurant group, Gordon Ramsay Restaurants, in 1997.")
+                .WithFooter("Source: Wikipedia")
+                .WithColor(Colours.Blue)
+                .WithThumbnailUrl("http://www.gstatic.com/tv/thumb/persons/319794/319794_v9_bb.jpg")
+                .WithUrl("https://www.gordonramsay.com")
+                .Build();
+            await ReplyAsync("", false, builder.Build());
+        }
+
+        // suggest
+        [Command("suggest")]
+        public async Task SuggestSomethingPingArrow([Remainder]string suggestion)
+        {
+            var client = Context.Client;
+            ulong channelID = 728918609027661828;
+            var c = client.GetChannel(channelID) as SocketTextChannel;
+            await c.SendMessageAsync($"<@470033984575897600> New suggestion: {suggestion}\nFrom: {Context.User.Mention}");
+            await Context.Channel.SendMessageAsync($"Your suggestion: ``{suggestion}`` has been sent to the bot developer.");
+        }
+
+        //help menu
+        [Command("help")]
+        public async Task DisplayHelp()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.WithTitle("Help!")
+                .WithColor(Colours.Blue)
+                .AddField("!suggest", "Submit something you want in the bot")
+                .AddField("!bio", "Displays Gordon's Bio")
+                .AddField("!insult @user", "Use a famous line to insult someone!")
+                .AddField("!image", "Displays a random image of Gordon")
+                .AddField("!trivia", "Shows trivia menu")
+                .AddField("!trivia solo", "Play trivia solo")
+                .AddField("!trivia all", "First to answer wins");
+            builder.Build();
+            await ReplyAsync("", false, builder.Build());
+        }
     }
 }
