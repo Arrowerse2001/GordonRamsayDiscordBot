a8a5ef88aa03e3e796d2df46b23298fcce8e75fc
William <WilliamWelsh@users.noreply.github.com>
2021-02-24 15:17:31 -0500

    Added IsBotOwner precondition, and made coding style more consistent, and removed duplicate !serverstats command

--- Gordon Ramsay Discord Bot/GordonRamsayDiscordBot/Commands/Insults.cs
+++ Gordon Ramsay Discord Bot/GordonRamsayDiscordBot/Commands/Insults.cs
@@ -2,6 +2,7 @@
 using Discord.Commands;
 using System.Text;
 using System.Threading.Tasks;
+using GordonRamsayDiscordBot;
 
 namespace GordonRamsayBot.Commands
 {
@@ -9,105 +10,93 @@ namespace GordonRamsayBot.Commands
     [Group("insults")]
     public class Insults : ModuleBase
     {
+        [IsBotOwner]
         [Command("add")]
-        public async Task AddNewInsult([Remainder]string insult)
+        public async Task AddNewInsult([Remainder] string insult)
         {
-            if (Context.User.Id != 470033984575897600)
+            // Add new insult to list!
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(@"MasterList.txt", true))
             {
-                await Context.Channel.SendMessageAsync("Only the bot developer can do that. Use ``gr!suggest``");
-            } else
-            {
-                // Add new insult to list!
-                using (System.IO.StreamWriter file =
-                    new System.IO.StreamWriter(@"MasterList.txt", true))
-                {
-                    file.Write($"\n{insult}");
-                    
-                }
-
-                // Add to in progress
-                using (System.IO.StreamWriter file =
-                    new System.IO.StreamWriter(@"insultsProgress", true))
-                {
-                    file.Write($"\n{insult}");
-                }
-
-                // Reload
-                GordonRamsayBot.ArrayHandler.ReloadInsults();
-                GordonRamsayBot.ArrayHandler.ReloadMasterList();
-                await Context.Channel.SendMessageAsync($"New insult added: {insult}");
+                file.Write($"\n{insult}");
             }
+
+            // Add to in progress
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(@"insultsProgress", true))
+            {
+                file.Write($"\n{insult}");
+            }
+
+            // Reload
+            ArrayHandler.ReloadInsults();
+            ArrayHandler.ReloadMasterList();
+            await Context.Channel.SendMessageAsync($"New insult added: {insult}");
         }
 
         [Command("count")]
         public async Task Count()
         {
-            int c = GordonRamsayBot.ArrayHandler.MasterList.Count;
-            int ic = GordonRamsayBot.ArrayHandler.gInsults.Count;
+            var c = ArrayHandler.MasterList.Count;
+            var ic = ArrayHandler.gInsults.Count;
             await Context.Channel.SendMessageAsync($"Total insults: {c}\nRemaining Insults: {ic}");
         }
 
+        [IsBotOwner]
         [Command("delete")]
         public async Task DeleteInsult([Remainder] string insult)
         {
-            if (Context.User.Id != 470033984575897600)
+            var ip = false;
+            var ml = false;
+            for (int i = 0; i < ArrayHandler.gInsults.Count; i++)
             {
-                await Context.Channel.SendMessageAsync($"Only the bot developer can do that.");
+                if (!ip && ArrayHandler.gInsults[i].ToLower().Equals(insult.ToLower()))
+                {
+                    ip = true;
+                    ArrayHandler.gInsults.RemoveAt(i);
+                    ArrayHandler.RewriteInsults();
+                    await Context.Channel.SendMessageAsync($"Insult deleted from list!");
+                    break;
+                }
             }
+
+            for (int i = 0; i < ArrayHandler.MasterList.Count; i++)
+            {
+                if (!ml && ArrayHandler.MasterList[i].ToLower().Equals(insult.ToLower()))
+                {
+                    ml = true;
+                    ArrayHandler.MasterList.RemoveAt(i);
+                    ArrayHandler.RewriteMasterListInsults();
+                    await Context.Channel.SendMessageAsync($"Insult deleted from master list");
+                    break;
+                }
+            }
+
+            var sb = new StringBuilder();
+            if (!ip || !ml)
+                sb.Append("That insult wasn't found" + (!ip && !ml ? " in either list." : ml ? " in the in-progress list." : " in the master list."));
             else
-            {
-                bool ip = false;
-                bool ml = false;
-                for (int i = 0; i < ArrayHandler.gInsults.Count; i++)
-                {
-                    if (!ip && GordonRamsayBot.ArrayHandler.gInsults[i].ToLower().Equals(insult.ToLower()))
-                    {
-                        ip = true;
-                        ArrayHandler.gInsults.RemoveAt(i);
-                        ArrayHandler.RewriteInsults();
-                        await Context.Channel.SendMessageAsync($"Insult deleted from list!");
-                        break;
-                    }
-                }
-
-                for (int i = 0; i < ArrayHandler.MasterList.Count; i++)
-                {
-                    if (!ml && ArrayHandler.MasterList[i].ToLower().Equals(insult.ToLower()))
-                    {
-                        ml = true;
-                        ArrayHandler.MasterList.RemoveAt(i);
-                        ArrayHandler.RewriteMasterListInsults();
-                        await Context.Channel.SendMessageAsync($"Insult deleted from master list");
-                        break;
-                    }
-                }
-
-                StringBuilder sb = new StringBuilder();
-                if (!ip || !ml)
-                    sb.Append("That insult wasn't found" + (!ip && !ml ? " in either list." : ml ? " in the in-progress list." : " in the master list."));
-                else
-                    sb.Append($"New insult count:\nTotal: {ArrayHandler.MasterList.Count}\nIn-Progress: {ArrayHandler.gInsults.Count}");
-                await Context.Channel.SendMessageAsync(sb.ToString());
-            }
+                sb.Append($"New insult count:\nTotal: {ArrayHandler.MasterList.Count}\nIn-Progress: {ArrayHandler.gInsults.Count}");
+            await Context.Channel.SendMessageAsync(sb.ToString());
         }
 
         [Command("find")]
-        public async Task FindInsult([Remainder]string q)
+        public async Task FindInsult([Remainder] string q)
         {
-            StringBuilder sb = new StringBuilder();
-            int i = 0;
+            var sb = new StringBuilder();
+            var i = 0;
             for (int ii = 0; ii < ArrayHandler.MasterList.Count; ii++)
             {
-                string s = ArrayHandler.MasterList[ii];
+                var s = ArrayHandler.MasterList[ii];
                 if (s.ToLower().Contains(q.ToLower()))
                     sb.Append($"{++i}. {s}\n");
             }
-            // Embed
-            EmbedBuilder builder = new EmbedBuilder();
-            builder.WithTitle($"Insults containing {q}")
+
+            await ReplyAsync("", false, new EmbedBuilder()
+                .WithTitle($"Insults containing {q}")
                 .WithDescription(sb.ToString())
-                .WithColor(Colours.Blue);
-            await ReplyAsync("", false, builder.Build());
+                .WithColor(Colours.Blue)
+                .Build());
         }
     }
 }
--- Gordon Ramsay Discord Bot/GordonRamsayDiscordBot/Commands/Misc.cs
+++ Gordon Ramsay Discord Bot/GordonRamsayDiscordBot/Commands/Misc.cs
@@ -1,22 +1,19 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GordonRamsayBot.Commands
 {
-
     [RequireContext(ContextType.Guild)]
     public class Misc : ModuleBase<SocketCommandContext>
     {
         // Ping to see if the bot is on and will reply back
         [Command("ping")]
-        public async Task PingPong() => await Context.Channel.SendMessageAsync($"Pong!");
+        public async Task PingPong() => await Context.Channel.SendMessageAsync("Pong!");
 
         // Insult someone by using a Gordon Ramsay Quote
         [Command("insult")]
@@ -24,7 +21,8 @@ namespace GordonRamsayBot.Commands
         {
             if (user == null)
                 user = (SocketGuildUser)Context.User;
-            int iN = Utilities.GetRandomNumber(0, ArrayHandler.gInsults.Count);
+
+            var iN = Utilities.GetRandomNumber(0, ArrayHandler.gInsults.Count);
             var insult = ArrayHandler.GetInsult(iN);
             await Context.Channel.SendMessageAsync($"{user.Mention} {insult}");
         }
@@ -33,38 +31,34 @@ namespace GordonRamsayBot.Commands
         [Command("image")]
         public async Task SendGordonImage()
         {
-            int imageNum = Utilities.GetRandomNumber(0, GordonRamsayDiscordBot.Handlers.ImageHander.images.Count);
+            var imageNum = Utilities.GetRandomNumber(0, GordonRamsayDiscordBot.Handlers.ImageHander.images.Count);
             var image = GordonRamsayDiscordBot.Handlers.ImageHander.GetGordonImage(imageNum);
 
-            EmbedBuilder b = new EmbedBuilder();
-            b.WithImageUrl(image)
-                .WithColor(Colours.Blue);
-            b.Build();
-            await ReplyAsync("", false, b.Build());
+            await ReplyAsync("", false, new EmbedBuilder()
+                .WithImageUrl(image)
+                .WithColor(Colours.Blue)
+                .Build());
         }
 
         // trivia
         [Command("trivia")]
         public async Task TryToStartTrivia(string input = null) => await Handlers.MinigameHandler.Trivia.TryToStartTrivia((SocketGuildUser)Context.User, Context, input ?? "trivia");
-    
+
         // Bio
         [Command("bio")]
-        public async Task DisplayGordonBio()
-        {
-            EmbedBuilder builder = new EmbedBuilder();
-            builder.WithTitle("Gordon Ramsay")
+        public async Task DisplayGordonBio() =>
+            await ReplyAsync("", false, new EmbedBuilder()
+                .WithTitle("Gordon Ramsay")
                 .WithDescription("Gordon James Ramsay OBE is a British chef, restaurateur, television personality, and writer. Born in Johnstone, Scotland, and raised in Stratford-upon-Avon, England, he founded his global restaurant group, Gordon Ramsay Restaurants, in 1997.")
                 .WithFooter("Source: Wikipedia")
                 .WithColor(Colours.Blue)
                 .WithThumbnailUrl("http://www.gstatic.com/tv/thumb/persons/319794/319794_v9_bb.jpg")
                 .WithUrl("https://www.gordonramsay.com")
-                .Build();
-            await ReplyAsync("", false, builder.Build());
-        }
+                .Build());
 
         // suggest
         [Command("suggest")]
-        public async Task SuggestSomethingPingArrow([Remainder]string suggestion)
+        public async Task SuggestSomethingPingArrow([Remainder] string suggestion)
         {
             var client = Context.Client;
             ulong channelID = 728918609027661828;
@@ -75,10 +69,9 @@ namespace GordonRamsayBot.Commands
 
         //help menu
         [Command("help")]
-        public async Task DisplayHelp()
-        {
-            EmbedBuilder builder = new EmbedBuilder();
-            builder.WithTitle("Help!")
+        public async Task DisplayHelp() =>
+            await ReplyAsync("", false, new EmbedBuilder()
+                .WithTitle("Help!")
                 .WithColor(Colours.Blue)
                 .AddField("gr!suggest", "Submit something you want in the bot")
                 .AddField("gr!bio", "Displays Gordon's Bio")
@@ -89,10 +82,8 @@ namespace GordonRamsayBot.Commands
                 .AddField("gr!trivia solo", "Play trivia solo")
                 .AddField("gr!trivia all", "First to answer wins")
                 .AddField("gr!serverstats", "Show server stats")
-                .AddField("gr!stats", "Display Bot Stats");
-            builder.Build();
-            await ReplyAsync("", false, builder.Build());
-        }
+                .AddField("gr!stats", "Display Bot Stats")
+                .Build());
 
         // GitHub
         [Command("github")]
@@ -109,20 +100,17 @@ namespace GordonRamsayBot.Commands
         // Server Stats
         [Command("serverstats")]
         [Alias("server stats", "serverinfo", "server info")]
-        public async Task DisplaySeverStats()
-        {
-            EmbedBuilder builder = new EmbedBuilder();
-            builder.WithTitle(Context.Guild.Name);
-            builder.AddField("Created", Context.Guild.CreatedAt.ToString("dddd, MMMM d, yyyy"));
-            builder.AddField("Owner", Context.Guild.Owner.Mention);
-            builder.AddField("Emotes", Context.Guild.Emotes.Count);
-            builder.AddField("Members", Context.Guild.MemberCount.ToString("#,##0"));
-            builder.WithThumbnailUrl(Context.Guild.IconUrl);
-            builder.WithColor(Colours.Blue);
-            builder.Build();
-            await ReplyAsync("", false, builder.Build());
-        }
-        
+        public async Task DisplaySeverStats() =>
+            await ReplyAsync("", false, new EmbedBuilder()
+                .WithTitle(Context.Guild.Name)
+                .AddField("Created", Context.Guild.CreatedAt.ToString("dddd, MMMM d, yyyy"))
+                .AddField("Owner", Context.Guild.Owner.Mention)
+                .AddField("Emotes", Context.Guild.Emotes.Count)
+                .AddField("Members", Context.Guild.MemberCount.ToString("#,##0"))
+                .WithThumbnailUrl(Context.Guild.IconUrl)
+                .WithColor(Colours.Blue)
+                .Build());
+
         // Get user avatar
         [Command("avatar")]
         [Alias("av", "avi")]
@@ -136,28 +124,28 @@ namespace GordonRamsayBot.Commands
         // Find People with the desired role
         private string GetMembersWithRole(string DesiredRole)
         {
-            SocketRole role = Context.Guild.Roles.FirstOrDefault(x => x.Name == DesiredRole);
-            StringBuilder d = new StringBuilder();
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == DesiredRole);
+            var d = new StringBuilder();
+
             foreach (SocketGuildUser user in Context.Guild.Users.ToArray())
-            {
                 if (user.Roles.Contains(role))
                     d.AppendLine($"{user.Mention}");
-            }
+
             return d.ToString();
         }
 
         // Command to see who has the role (role is case sensitive)
         [Command("members")]
-        public async Task FindPeopleInRoles([Remainder]string role) => await Utilities.SendEmbed(Context.Channel, $"Members", GetMembersWithRole(role), Colours.Blue, "", "");
+        public async Task FindPeopleInRoles([Remainder] string role) => await Utilities.SendEmbed(Context.Channel, $"Members", GetMembersWithRole(role), Colours.Blue, "", "");
 
         // Reply to a message
         [Command("reply")]
         public async Task Reply(ISocketMessageChannel channel, ulong msg, [Remainder] string reply)
         {
-            SocketGuildUser cu = (SocketGuildUser)Context.User;
+            var cu = (SocketGuildUser)Context.User;
             if (cu.GuildPermissions.ManageMessages == false)
             {
-                await Context.Channel.SendMessageAsync($"I know you may be slightly stupid but you do not have permission to use this command!");
+                await Context.Channel.SendMessageAsync("I know you may be slightly stupid but you do not have permission to use this command!");
             }
             else
             {
@@ -165,34 +153,17 @@ namespace GordonRamsayBot.Commands
                 ulong channelID = channel.Id;
                 var c = client.GetChannel(channelID) as SocketTextChannel;
 
-                MessageReference m = new MessageReference(msg);
+                var m = new MessageReference(msg);
 
                 await c.SendMessageAsync(reply, false, null, null, null, m);
             }
-        }
-
-        // Display server stats
-        [Command("severstats")]
-        [Alias("server stats", "serverinfo", "server info")]
-        public async Task DisplayServerStats()
-        {
-            EmbedBuilder builder = new EmbedBuilder();
-            builder.WithTitle($"{Context.Guild.Name}'s Server Stats")
-                .WithThumbnailUrl(Context.Guild.IconUrl)
-                .WithColor(Colours.Blue)
-                .AddField("Created", Context.Guild.CreatedAt.ToString("dddd, MMMM d, yyyy"))
-                .AddField("Owner: ", $"{Context.Guild.Owner.Mention}")
-                .AddField("Emotes: ", $"{Context.Guild.Emotes.Count}")
-                .AddField("Members: ", $"{Context.Guild.MemberCount}");
-            builder.Build();
-            await ReplyAsync("", false, builder.Build());
         }
 
         // Say something to a specific channel
         [Command("say")]
         public async Task SaySomething(ISocketMessageChannel channel, [Remainder] string msg)
         {
-            SocketGuildUser cu = (SocketGuildUser)Context.User;
+            var cu = (SocketGuildUser)Context.User;
             if (cu.GuildPermissions.ManageMessages == false)
             {
                 await Context.Channel.SendMessageAsync($"I know you may be slightly stupid but you do not have permission to use this command!");
@@ -244,7 +215,7 @@ namespace GordonRamsayBot.Commands
                     }
                 }
                 await user.SendMessageAsync($"You have been banned!\nServer: {Context.Guild.Name}\nBy: {Context.User}\nReason: {reason}");
-                await Context.Guild.AddBanAsync(user, 0, reason, null);
+                await Context.Guild.AddBanAsync(user, 0, reason);
                 await Context.Channel.SendMessageAsync($"{user} has been banned by {Context.User.Mention} for {reason}.");
             }
         }
